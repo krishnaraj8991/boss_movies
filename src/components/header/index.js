cabf0c85b506f771d909748898b7f8ff435f82b7
ajayshewale27 <ajayshewale27@gmail.com>
2021-05-31 13:27:55 +0530

    css added for header and movie tile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,6 +6,7 @@ function Header() {
     <nav className='header'>
       <div>
         <h2 id='boss'>BOSS MOVIES</h2>
+        <p id='subt'> must watch movies from the ’80s</p>
       </div>
     </nav>
   );
